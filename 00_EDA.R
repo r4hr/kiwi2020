7cf2307ad72bf888d82cbc4982abc248e7740f3b
r4hr <info@d4hr.com>
2020-10-23 03:04:25 -0300

    Creo slide de gap salarial y me voy a dormir.

--- 00_EDA.R
+++ 00_EDA.R
@@ -25,6 +25,8 @@ estilo <- theme(panel.grid = element_blank(),
 
 genero <- c("#8624F5", "#1FC3AA", "#FFD129", "#75838F") #Violeta - Verde - Amarillo - Gris
 
+colores <-  c("#8624F5", "#1FC3AA")
+
 fuente <- "Fuente: Encuesta KIWI de Sueldos de RRHH para Latam"
 
 
@@ -426,4 +428,125 @@ ggplot(nombres, aes(x = rtas, y = reorder(nombre,rtas))) +
   labs(title = "Top 10 de nombres para el área de RRHH", 
        x = "", y = "", caption = fuente)
 
+# Brecha salarial prueba -------------------------------------------------
+library(eph)
+library(ggthemes)
+
+
+base_individual <- get_microdata(year = 2019, trimester = 3, type = "individual")
+
+base_individual <- base_individual %>% 
+  mutate(Sexo = as.character(CH04),
+         Sexo = case_when(Sexo=="1" ~ "Varones",
+                          Sexo=="2" ~ "Mujeres"),
+         PP04D_COD = as.character(PP04D_COD),
+         PP04D_COD = case_when(nchar(PP04D_COD) == 5 ~ PP04D_COD,
+                               nchar(PP04D_COD) == 4 ~ paste0("0", PP04D_COD),
+                               nchar(PP04D_COD) == 3 ~ paste0("00", PP04D_COD),
+                               nchar(PP04D_COD) == 2 ~ paste0("000", PP04D_COD),
+                               nchar(PP04D_COD) == 1 ~ paste0("0000", PP04D_COD)),
+         CALIFICACION = substr(PP04D_COD, 5, 5),
+         CALIFICACION = case_when(CALIFICACION=="1" ~ "Profesionales",
+                                  CALIFICACION=="2" ~ "Técnicos",
+                                  CALIFICACION=="3" ~ "Operativos",
+                                  CALIFICACION=="4" ~ "No Calificados",
+                                  TRUE ~ "0"),
+         CALIFICACION = factor(CALIFICACION, c("No Calificados", "Operativos", "Técnicos", "Profesionales")),
+         JERARQUIA = substr(PP04D_COD, 3, 3),
+         JERARQUIA = case_when(JERARQUIA %in% c("0", "2") ~ "Dirección o Jefes",
+                               JERARQUIA=="1" ~ "Cuentapropia",
+                               JERARQUIA=="3" ~ "Trabajadores Asalariados",
+                               TRUE ~ "0"),
+         JERARQUIA = factor(JERARQUIA, c("Dirección o Jefes", "Trabajadores Asalariados", "Cuentapropia")),
+         NIVEL_EDUCATIVO = case_when(NIVEL_ED %in% c(1, 7) ~ "Sin Instrucción",
+                                     NIVEL_ED %in% c(2, 3) ~ "Primaria",
+                                     NIVEL_ED %in% c(4, 5) ~ "Secundaria",
+                                     NIVEL_ED == 6         ~ "Superior",
+                                     NIVEL_ED == 9         ~ "NS/NR"),
+         NIVEL_EDUCATIVO = factor(NIVEL_EDUCATIVO, levels = c("Sin Instrucción", "Primaria", "Secundaria", "Superior")),
+         GRUPO_EDAD = case_when(CH06 >= 14 & CH06 <= 29 ~ "de 14 a 29 años",
+                                CH06 >= 30 & CH06 <= 64 ~ "de 30 a 64 años"))
+
+# colores = c("#aa165a","#16aa66")
+colores = c("#FE1764", "#00BDD6")
+
+# Ocupades
+tabla8 <- base_individual %>% 
+  filter(CALIFICACION != "0",
+         ESTADO == 1) %>% 
+  group_by(Sexo, CALIFICACION) %>% 
+  summarise(IOP_mensual  = round(weighted.mean(P21, PONDIIO), 2)) 
+
+
+tabla8_graf <- tabla8 %>% 
+  pivot_wider(., names_from = Sexo, values_from = IOP_mensual) %>% 
+  mutate(brecha = percent((Varones-Mujeres)/Varones, 1),
+         x = (Varones+Mujeres)/2)
+
+tabla8_graf
+
+library(ggalt)
+
+ggplot(tabla8_graf, 
+       aes(x = Mujeres, xend = Varones, y = CALIFICACION, 
+           group = CALIFICACION, label = brecha)) +
+  geom_dumbbell(color = "#808080",
+                size_x = 3, size_xend = 3,
+                colour_x = colores[1],
+                colour_xend = colores[2]) +
+  geom_text(data = tabla8_graf, 
+            aes(x, CALIFICACION, label = brecha), nudge_y = .2) +
+  labs(title = "Brecha de ingresos de la ocupación principal
+       por sexo y calificación ocupacional",
+       x = "Ingreso Mensual",
+       y = NULL, 
+       caption = fuente) +
+  scale_color_manual(values = colores) +
+  theme_minimal()
+ 
+
+# Brecha salarial posta ------------------------------------
+
+liderazgo %>% 
+  group_by(puesto) %>% 
+  summarise(total = sum(cuenta)) %>% 
+  arrange(-total)
+
+brecha <- liderazgo %>% 
+  filter(genero %in% c("Femenino", "Masculino"), 
+         puesto %in% c("Director", "Gerente", "HRBP","Responsable", "Analista", "Administrativo")) %>% 
+  mutate(puesto = factor(puesto, levels = c("Administrativo","Analista", 
+                                            "HRBP", "Responsable","Gerente","Director" ))) %>% 
+  select(-pais) %>% 
+  group_by(genero, puesto) %>% 
+  summarise(media_salarial = mean(sueldo))
+
+
+
+brecha_graf <- brecha %>% 
+  pivot_wider(., names_from = genero, values_from = media_salarial) %>% 
+  mutate(brecha = percent((Masculino-Femenino)/Masculino, 1),
+         x = (Masculino + Femenino)/2)
+
+brecha_graf
+
+ggplot(brecha_graf, 
+       aes(x = Femenino, xend = Masculino, y = puesto, 
+           group = puesto, label = brecha)) +
+  geom_dumbbell(color = "#808080",
+                size_x = 3, size_xend = 3,
+                colour_x = colores[1],
+                colour_xend = colores[2]) +
+  geom_text(data = brecha_graf, 
+            aes(x, puesto, label = brecha), nudge_y = .2) +
+  labs(title = "Brecha salarial por puestos",
+       subtitle = "Sueldos promedios en Argentina",
+       x = "",
+       y = NULL, 
+       caption = fuente) +
+  scale_x_continuous(labels = comma_format(big.mark = ".", decimal.mark = ",")) +
+  scale_color_manual(values = colores) +
+  theme_minimal()+
+  theme(plot.background = element_rect(fill = "#fbfcfc"),
+        text = element_text(family = "Roboto"))
 
